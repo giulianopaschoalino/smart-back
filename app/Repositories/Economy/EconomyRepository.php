e225ab62c4416bedad7a25cbf36328bf5998077d
Luiz Filipe <luizfilipe@klupp.com.br>
2022-08-16 02:16:27 -0300

    Only current year

--- app/Repositories/Economy/EconomyRepository.php
+++ app/Repositories/Economy/EconomyRepository.php
@@ -94,6 +94,9 @@ class EconomyRepository extends AbstractRepository implements EconomyContractInt
             ->where(DB::raw("TO_DATE(economia.mes, 'YYMM')"),
                 ">=",
                 DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-01-01'), 'YYYY-MM-DD')"))
+            ->where(DB::raw("TO_DATE(economia.mes, 'YYMM')"),
+            "<=",
+            DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-12-31'), 'YYYY-MM-DD')"))
             ->groupBy(['mes', 'dad_estimado'])
             ->orderBy(DB::raw("mes, dad_estimado"))
             ->havingRaw("sum(custo_livre) > 0")
