7ac06b6dd6a2f552807971e3bdd51f97d205c193
Giuliano Paschoalino <back5@smartenergia.com.br>
2026-01-19 13:55:28 -0300

    Fix date filtering in getGrossMonthlyEconomy and getCaptiveMonthlyEconomy methods for accurate data retrieval

--- app/Repositories/Economy/EconomyRepository.php
+++ app/Repositories/Economy/EconomyRepository.php
@@ -93,7 +93,7 @@ class EconomyRepository extends AbstractRepository implements EconomyContractInt
                 DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-01-01'), 'YYYY-MM-DD') - interval '2' year"))
             ->where(DB::raw("TO_DATE(economia.mes, 'YYMM')"),
             "<=",
-            DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-12-31'), 'YYYY-MM-DD')"))
+            DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-MM-DD'), 'YYYY-MM-DD')"))
             ->groupBy(['mes', 'dad_estimado'])
             ->orderBy(DB::raw("mes, dad_estimado"))
             ->havingRaw("sum(custo_livre) > 0")
@@ -122,7 +122,7 @@ class EconomyRepository extends AbstractRepository implements EconomyContractInt
                 DB::raw("TO_DATE(economia.mes, 'YYMM')"),
                 [
                     DB::raw("TO_DATE(TO_CHAR(current_date , 'YYYY-01-01'), 'YYYY-MM-DD') - interval '2' year"),
-                    DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-12-31'), 'YYYY-MM-DD') ")
+                    DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-MM-DD'), 'YYYY-MM-DD') ")
                 ])
             // ->whereRaw("TO_DATE(economia.mes, 'YYMM') >= TO_DATE(TO_CHAR(current_date , 'YYYY-01-01'), 'YYYY-MM-DD') - INTERVAL '0' year")
             ->groupBy(['mes', 'dad_estimado'])
