5befe2d4f13a616b57ca94f8cf8c75dce891d54c
Giuliano Paschoalino <back5@smartenergia.com.br>
2026-04-28 09:57:16 -0300

    Refactor date range in getCaptiveMonthlyEconomy for improved accuracy

--- app/Repositories/Economy/EconomyRepository.php
+++ app/Repositories/Economy/EconomyRepository.php
@@ -129,8 +129,8 @@ class EconomyRepository extends AbstractRepository implements EconomyContractInt
             ->whereBetween(
                 DB::raw("TO_DATE(economia.mes, 'YYMM')"),
                 [
-                    DB::raw("TO_DATE(TO_CHAR(current_date , 'YYYY-MM-DD'), 'YYYY-MM-DD') - interval '13' month"),
-                    DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-MM-DD'), 'YYYY-MM-DD') ")
+                    DB::raw("TO_DATE(TO_CHAR(current_date , 'YYYY-01-01'), 'YYYY-MM-DD') - interval '2' year"),
+                    DB::raw("TO_DATE(TO_CHAR(current_date, 'YYYY-12-31'), 'YYYY-MM-DD') ")
                 ])
             // ->whereRaw("TO_DATE(economia.mes, 'YYMM') >= TO_DATE(TO_CHAR(current_date , 'YYYY-01-01'), 'YYYY-MM-DD') - INTERVAL '0' year")
             ->groupBy(['mes', 'dad_estimado'])
